ecb66c34624d0ba8a9c7f4f2807faf277048c112
Roldão Junior <roldaogjr@gmail.com>
2018-08-19 14:20:33 -0300

    Ajustes nas configurações

--- cbvadmin_semantic_ui/settings.py
+++ cbvadmin_semantic_ui/settings.py
@@ -1,15 +1,13 @@
-from . import REQUIRED_APPS
-
 required_settings = {
     'CBVADMIN_TEMPLATE_PACK': 'semantic-ui',
     'CRISPY_TEMPLATE_PACK': 'semantic-ui',
-    'CRISPY_ALLOWED_TEMPLATE_PACKS': ('semantic-ui',)
+    'CRISPY_ALLOWED_TEMPLATE_PACKS': ('semantic-ui',),
+    'CRISPY_CLASS_CONVERTERS': {
+        #'select': 'ui selection fluid dropdown'
+    }
 }
 
 
-def cbvadmin_semantic_ui_settings(settings):
+def update_cbvadmin_settings(settings):
     settings.update(required_settings)
-    if 'INSTALLED_APPS' not in settings:
-        settings['INSTALLED_APPS'] = []
-    settings['INSTALLED_APPS'] += REQUIRED_APPS
     return settings
